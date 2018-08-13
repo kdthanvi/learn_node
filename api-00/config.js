90053d2b61fb9bdd63ab05827de7c724f4ed6001
kdthanvi <thanvikaran95@gmail.com>
2018-08-13 18:11:40 +0900

    heroku-configs

--- api-00/config.js
+++ api-00/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    slack_ws : 'XXXX', //your workspace name goes here
-    slack_token : 'xoxp-XXXX' //your legacy token goes here
+    slack_ws : process.env.slack_ws || 'XXXX', //your workspace name goes here
+    slack_token : process.env.slack_token || 'xoxp-XXXX' //your legacy token goes here
 
 };
